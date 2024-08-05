42c3e39ead59d39e5ac846b4ab82b10a1a936940
Pau Milà Guasch <pau.mila@sdggroup.com>
2024-08-05 08:58:34 +0000

    customs tests

--- models/staging/jaffle/stg_jaffle_orders.sql
+++ models/staging/jaffle/stg_jaffle_orders.sql
@@ -1,10 +1,17 @@
 select
-    {{ dbt_utils.surrogate_key(['user_id','order_date'])}} as id,
+    /*{{ dbt_utils.surrogate_key(['user_id','order_date'])}} as id,*/
     id as order_id,
     user_id as customer_id,
     order_date,
-    status
+    date_diff({{ dbt.current_timestamp() }}, order_date, day ) as days_since_ordered,
+    status like '%pending%' as is_status_pending,
+    case
+        when status like '%shipped%' then 'shipped'
+        when status like '%return%' then 'returned'
+        when status like '%pending%' then 'placed'
+        else status
+    end as status
 
 from {{ source('jaffle_shop', 'orders') }}
 
-{{ limit_data_in_devs(column_name='order_date', dev_days_of_data=10000) }}
+/*{{ limit_data_in_devs(column_name='order_date', dev_days_of_data=10000) }}*/
